397c2803015e26cae6ea3f86feca4fb535711282
fantonio <antonio@advancedblockchain.com>
2022-02-28 14:26:03 +0100

    Generating keys+

--- scripts/generateKeys.sh
+++ scripts/generateKeys.sh
@@ -3,35 +3,38 @@ if [ $# -lt 0 ];then
   echo "Use: $(basename "$0") [key_size] [n_keys]"
   echo "It generates the required RSA keys for ARA2 access mode"
   echo "Example: generateKeys.sh ./demo"
+  exit
 fi
 
 # Key size
-if [ $# -lt 1 ];then
-  keySize=128
-else
+if [ $# -lt 0 ];then
   keySize=$1
+else
+  keySize=128
 fi
 
 # Number of keys to generate
-if [ $# -lt 2 ];then
-  numberKeys=10
-else
+if [ $# -lt 1 ];then
   numberKeys=$2
+else
+  numberKeys=10
 fi
 
+echo $keySize
 # Target directory
-targetDirectory=/keys/${keySize}/
+targetDirectory=/keys/"$keySize"/
+echo $targetDirectory
 
 # Create target folder if it does not exist.
-if [ ! -d targetDirectory ]; then
-    mkdir -p targetDirectory
+if [ ! -d ${targetDirectory} ]; then
+    mkdir -p ${targetDirectory}
 fi
 
 # Change directory
 cd targetDirectory || exit
-
-for i in {1..10};
+for i in $(seq 1 ${numberKeys});
 do
-  openssl genrsa -out private-key-${i}.pem keySize  # Generate private key
+  echo $keySize
+  openssl genrsa -out private-key-${i}.pem ${keySize}  # Generate private key
   openssl rsa -in private-key-${i}.pem -pubout -out public-key-${i}.pem # Generate associated public key
 done
